566ef77dcede9ce2ab6622db660cfa4ba39997f5
jake23hogs <gilesj@greenbrierschools.org>
2018-06-06 20:54:22 -0500

    Update __init__.py

--- cs50/2017/x/isPrime/check50/__init__.py
+++ cs50/2017/x/isPrime/check50/__init__.py
@@ -21,6 +21,21 @@ class isPrime(Checks):
         self.spawn("./isPrime").stdin("2").stdout("true\n", "true\n").exit(0)
 
     @check("compiles")
+    def test27(self):
+        """input of 27 yields output of false"""
+        self.spawn("./isPrime").stdin("27").stdout("false\n", "false\n").exit(0)
+
+    @check("compiles")
+    def test101(self):
+        """input of 101 yields output of true"""
+        self.spawn("./isPrime").stdin("101").stdout("true\n", "true\n").exit(0)
+
+    @check("compiles")
+    def test121(self):
+        """input of 121 yields output of false"""
+        self.spawn("./isPrime").stdin("121").stdout("false\n", "false\n").exit(0)
+        
+    @check("compiles")
     def test_reject_negative(self):
         """rejects a negative input like -.1"""
         self.spawn("./isPrime").stdin("-1").reject()
