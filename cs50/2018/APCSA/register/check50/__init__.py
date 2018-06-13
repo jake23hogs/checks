3237d9e03940577c5f789963acd6473877d39dcb
jake23hogs <gilesj@greenbrierschools.org>
2018-06-12 23:04:22 -0500

    Update __init__.py

--- cs50/2018/APCSA/register/check50/__init__.py
+++ cs50/2018/APCSA/register/check50/__init__.py
@@ -3,69 +3,38 @@ import re
 from check50 import *
 
 
-class Greedy(Checks):
+class Register(Checks):
 
     @check()
     def exists(self):
-        """greedy exists"""
-        self.require("greedy.c")
+        """register exists"""
+        self.require("register.c")
 
     @check("exists")
     def compiles(self):
-        """greedy compiles"""
-        self.spawn("clang -std=c11 -ggdb3 -o greedy greedy.c -lcs50 -lm").exit(0)
+        """register compiles"""
+        self.spawn("clang -std=c11 -ggdb3 -o register register.c -lcs50 -lm").exit(0)
 
     @check("compiles")
-    def test041(self):
-        """input of 0.41 yields output of 4"""
-        self.spawn("./greedy").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+    def test2495(self):
+        """input of 24.95 yields outputs of 24.95, 1.80887, 26.7589"""
+        self.spawn("./register").stdin("24.95").stdout(24.95, "24.95\n").stdout(1.80887, "1.80887\n").stdout(26.7589, "26.7589\n").exit(0)
 
-    @check("compiles")
-    def test001(self):
-        """input of 0.01 yields output of 1"""
-        self.spawn("./greedy").stdin("0.01").stdout(coins(1), "1\n").exit(0)
-
-    @check("compiles")
-    def test015(self):
-        """input of 0.15 yields output of 2"""
-        self.spawn("./greedy").stdin("0.15").stdout(coins(2), "2\n").exit(0)
-
-    @check("compiles")
-    def test160(self):
-        """input of 1.6 yields output of 7"""
-        self.spawn("./greedy").stdin("1.6").stdout(coins(7), "7\n").exit(0)
-
-    @check("compiles")
-    def test230(self):
-        """input of 23 yields output of 92"""
-        self.spawn("./greedy").stdin("23").stdout(coins(92), "92\n").exit(0)
-
-    @check("compiles")
-    def test420(self):
-        """input of 4.2 yields output of 18"""
-        expected = "18\n"
-        actual = self.spawn("./greedy").stdin("4.2").stdout()
-        if not re.search(coins(18), actual):
-            err = Error(Mismatch(expected, actual))
-            if re.search(coins(22), actual):
-                err.helpers = "Did you forget to round your input to the nearest cent?"
-            raise err
-
+   
     @check("compiles")
     def test_reject_negative(self):
         """rejects a negative input like -.1"""
-        self.spawn("./greedy").stdin("-1").reject()
+        self.spawn("./register").stdin("-1").reject()
 
     @check("compiles")
     def test_reject_foo(self):
         """rejects a non-numeric input of "foo" """
-        self.spawn("./greedy").stdin("foo").reject()
+        self.spawn("./register").stdin("foo").reject()
 
     @check("compiles")
     def test_reject_empty(self):
         """rejects a non-numeric input of "" """
-        self.spawn("./greedy").stdin("").reject()
+        self.spawn("./register").stdin("").reject()
 
 
-def coins(num):
-    return r"(^|[^\d]){}(?!\d)".format(num)
+
