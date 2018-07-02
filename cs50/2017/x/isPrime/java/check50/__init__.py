f89cd86cc58710905c8b54fde2d04de19baddfc5
jake23hogs <gilesj@greenbrierschools.org>
2018-07-02 10:46:41 -0500

    Update __init__.py

--- cs50/2017/x/isPrime/java/check50/__init__.py
+++ cs50/2017/x/isPrime/java/check50/__init__.py
@@ -9,10 +9,10 @@ class isPrime(Checks):
         """isPrime exists"""
         self.require("isPrime.java")
 
-   #@check("exists")
-   #def compiles(self):
-   #    """isPrime compiles""" 
-   #    self.spawn("javac isPrime.java").exit(0)
+   @check("exists")
+   def compiles(self):
+       """isPrime compiles""" 
+       self.spawn("javac isPrime.java").exit(0)
 
     @check("exists")
     def test2(self):
