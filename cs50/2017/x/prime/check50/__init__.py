e9833678f31bad944caabd23647e34e6e3715109
jake23hogs <gilesj@greenbrierschools.org>
2018-07-20 11:49:32 -0500

    Update __init__.py

--- cs50/2017/x/prime/check50/__init__.py
+++ cs50/2017/x/prime/check50/__init__.py
@@ -2,24 +2,24 @@ import re
 
 from check50 import *
 
-class isPrime(Checks):
+class Prime(Checks):
 
     @check()
     def exists(self):
-        """isPrime exists"""
-        self.require("isPrime.java")
+        """Prime exists"""
+        self.require("Prime.java")
 
    @check("exists")
    def compiles(self):
-       """isPrime compiles""" 
-       self.spawn("javac isPrime.java").exit(0)
+       """Prime compiles""" 
+       self.spawn("javac Prime.java").exit(0)
 
     @check("exists")
     def test2(self):
         """input of 2 yields output of true"""
-        self.spawn("java isPrime").stdin("2").stdout("true\n", "true\n")
+        self.spawn("java Prime").stdin("2").stdout("true\n", "true\n")
 
     @check("exists")
     def test_reject_negative(self):
         """rejects a negative input like -1"""
-        self.spawn("java isPrime").stdin("-1").reject()
+        self.spawn("java Prime").stdin("-1").reject()
