28c23d330fd83daaf72aa32ea057c4ee66a9febb
jake23hogs <gilesj@greenbrierschools.org>
2018-07-20 13:02:46 -0500

    Update __init__.py

--- cs50/2017/x/demo/check50/__init__.py
+++ cs50/2017/x/demo/check50/__init__.py
@@ -1,20 +1,18 @@
 from check50 import *
 
 class Hello(Checks):
-
+    
     @check()
     def exists(self):
-        """Demo exists"""
+        """Hello exists"""
         self.require("Hello.java")
 
-   @check("exists")
-   def compiles(self):
+    @check("exists")
+    def compiles(self):
         """Hello compiles""" 
         self.spawn("javac Hello.java").exit(0)
 
     @check("compiles")
-    def test(self):
+    def test1(self):
         """displays Hello, World! correctly"""
         self.spawn("java Hello").stdout("Hello, World!\n", "Hello, World!\n")
-
-    
