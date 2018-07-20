029036214f7dd5e798ab13d36f836bf065502473
jake23hogs <gilesj@greenbrierschools.org>
2018-07-20 11:52:46 -0500

    Update __init__.py

--- cs50/2017/x/prime/check50/__init__.py
+++ cs50/2017/x/prime/check50/__init__.py
@@ -4,10 +4,10 @@ from check50 import *
 
 class Prime(Checks):
 
-    @check()
-    def exists(self):
-        """Prime exists"""
-        self.require("Primes.java")
+    #@check()
+    #def exists(self):
+     #   """Prime exists"""
+      #  self.require("Prime.java")
 
    @check("exists")
    def compiles(self):
