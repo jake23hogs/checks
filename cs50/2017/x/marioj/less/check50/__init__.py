141d4d89d87cf1b1a1f9d32206be44bb17304777
jake23hogs <gilesj@greenbrierschools.org>
2018-07-25 13:24:04 -0500

    Update __init__.py

--- cs50/2017/x/marioj/less/check50/__init__.py
+++ cs50/2017/x/marioj/less/check50/__init__.py
@@ -17,37 +17,62 @@ class MarioLess(Checks):
         self.spawn("javac MarioLess.java").exit(0)
 
     @check("compiles")
-    def test041(self):
-        """input of 0.41 yields output of 4"""
-        self.spawn("java MarioLess").stdin("0.41").stdout(coins(4), "4\n").exit(0)
-
-    @check("compiles")
-    def test001(self):
-        """input of 0.01 yields output of 1"""
-        self.spawn("java MarioLess").stdin("0.01").stdout(coins(1), "1\n").exit(0)
-
-    @check("compiles")
-    def test015(self):
-        """input of 0.15 yields output of 2"""
-        self.spawn("java MarioLess").stdin("0.15").stdout(coins(2), "2\n").exit(0)
-
-    @check("compiles")
-    def test160(self):
-        """input of 1.6 yields output of 7"""
-        self.spawn("java MarioLess").stdin("1.6").stdout(coins(7), "7\n").exit(0)
-
-    @check("compiles")
-    def test230(self):
-        """input of 23 yields output of 92"""
-        self.spawn("java MarioLess").stdin("23").stdout(coins(92), "92\n").exit(0)
-
-    @check("compiles")
     def test_reject_negative(self):
-        """rejects a negative input like -.1"""
+        """rejects a height of -1"""
         self.spawn("java MarioLess").stdin("-1").reject()
 
     @check("compiles")
+    def test0(self):
+        """handles a height of 0 correctly"""
+        self.spawn("java MarioLess").stdin("0").stdout(EOF).exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """handles a height of 1 correctly"""
+        out = self.spawn("java MarioLess").stdin("1").stdout()
+        correct = File("1.txt").read()
+        check_pyramid(out, correct)
+
+    @check("compiles")
+    def test2(self):
+        """handles a height of 2 correctly"""
+        out = self.spawn("java MarioLess").stdin("2").stdout()
+        correct = File("2.txt").read()
+        check_pyramid(out, correct)
+
+    @check("compiles")
+    def test23(self):
+        """handles a height of 23 correctly"""
+        out = self.spawn("java MarioLess").stdin("23").stdout()
+        correct = File("23.txt").read()
+        check_pyramid(out, correct)
+
+    @check("compiles")
+    def test24(self):
+        """rejects a height of 24, and then accepts a height of 2"""
+        self.spawn("java MarioLess").stdin("24").reject()\
+            .stdin("2").stdout(File("2.txt")).exit(0)
+
+    @check("compiles")
+    def test_reject_foo(self):
+        """rejects a non-numeric height of "foo" """
+        self.spawn("java MarioLess").stdin("foo").reject()
+
+    @check("compiles")
     def test_reject_empty(self):
-        """rejects a non-numeric input of "" """
+        """rejects a non-numeric height of "" """
         self.spawn("java MarioLess").stdin("").reject()
 
+
+def check_pyramid(output, correct):
+    if output == correct:
+        return
+
+    output = output.split("\n")
+    correct = correct.split("\n")
+
+    err = Error(Mismatch(correct, output))
+    # check if pyramids are the same height and only differ by trailing whitespace
+    if len(output) == len(correct) and all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
+        err.helpers = "Did you add too much trailing whitespace to the end of your pyramid?"
+    raise err
