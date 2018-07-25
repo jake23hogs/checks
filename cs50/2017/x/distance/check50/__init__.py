d43646b86606804701e3c646e270511c8f86f9d3
jake23hogs <gilesj@greenbrierschools.org>
2018-07-25 11:44:20 -0500

    Update __init__.py

--- cs50/2017/x/distance/check50/__init__.py
+++ cs50/2017/x/distance/check50/__init__.py
@@ -22,18 +22,13 @@ class Distance(Checks):
     
     @check("compiles")
     def distance1(self):
-        """inputs of x1 = 4, y1 = -1, x2 = -5, y2 = 3 yields Distance = 8.1"""
-        self.spawn("java Distance").stdin("4").stdin("-1").stdin("-5").stdin("3").stdout("Distance = 8.1\n", "Distance = 8.1\n").exit(0)
+        """inputs of x1 = -2, y1 = -1, x2 = 2, y2 = 1 yields Distance = 2.2"""
+        self.spawn("java Distance").stdin("-2").stdin("-1").stdin("2").stdin("1").stdout("Distance = 2.2\n", "Distance = 2.2\n").exit(0)
         
     @check("compiles")
     def test_reject_negative(self):
         """rejects a negative input like -.1"""
         self.spawn("java Distance").stdin("-1").reject()
-
-    @check("compiles")
-    def test_reject_foo(self):
-        """rejects a non-numeric input of "foo" """
-        self.spawn("java Distance").stdin("foo").reject()
 
     @check("compiles")
     def test_reject_empty(self):
