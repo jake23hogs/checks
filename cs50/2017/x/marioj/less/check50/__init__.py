5ebbda0be82a3d6eb413340dcbd16a66fb20d0d0
jake23hogs <gilesj@greenbrierschools.org>
2018-07-25 15:23:05 -0500

    Update __init__.py

--- cs50/2017/x/marioj/less/check50/__init__.py
+++ cs50/2017/x/marioj/less/check50/__init__.py
@@ -8,55 +8,55 @@ class MarioLess(Checks):
     @check()
     def exists(self):
         """MarioLess exists"""
-        self.require("MarioLess.java")
+        self.require("Mario.java")
         self.add("1.txt", "2.txt", "23.txt")
 
     @check("exists")
     def compiles(self):
         """MarioLess compiles"""
-        self.spawn("javac MarioLess.java").exit(0)
+        self.spawn("javac Mario.java").exit(0)
 
     @check("compiles")
     def test_reject_negative(self):
         """rejects a height of -1"""
-        self.spawn("java MarioLess").stdin("-1").reject()
+        self.spawn("java Mario").stdin("-1").reject()
 
     @check("compiles")
     def test0(self):
         """handles a height of 0 correctly"""
-        self.spawn("java MarioLess").stdin("0").stdout(EOF).exit(0)
+        self.spawn("java Mario").stdin("0").stdout(EOF).exit(0)
 
     @check("compiles")
     def test1(self):
         """handles a height of 1 correctly"""
-        out = self.spawn("java MarioLess").stdin("1").stdout()
+        out = self.spawn("java Mario").stdin("1").stdout()
         correct = File("1.txt").read()
         check_pyramid(out, correct)
 
     @check("compiles")
     def test2(self):
         """handles a height of 2 correctly"""
-        out = self.spawn("java MarioLess").stdin("2").stdout()
+        out = self.spawn("java Mario").stdin("2").stdout()
         correct = File("2.txt").read()
         check_pyramid(out, correct)
 
     @check("compiles")
     def test23(self):
         """handles a height of 23 correctly"""
-        out = self.spawn("java MarioLess").stdin("23").stdout()
+        out = self.spawn("java Mario").stdin("23").stdout()
         correct = File("23.txt").read()
         check_pyramid(out, correct)
 
     @check("compiles")
     def test24(self):
         """rejects a height of 24, and then accepts a height of 2"""
-        self.spawn("java MarioLess").stdin("24").reject()\
+        self.spawn("java Mario").stdin("24").reject()\
             .stdin("2").stdout(File("2.txt")).exit(0)
 
     @check("compiles")
     def test_reject_empty(self):
         """rejects a non-numeric height of "" """
-        self.spawn("java MarioLess").stdin("").reject()
+        self.spawn("java Mario").stdin("").reject()
 
 
 def check_pyramid(output, correct):
