4fc4db0620bf25ee050e4164bada87fd2a094fbf
jake23hogs <gilesj@greenbrierschools.org>
2018-07-31 11:17:55 -0500

    Update __init__.py

--- cs50/Competition/2018/first/check50/__init__.py
+++ cs50/Competition/2018/first/check50/__init__.py
@@ -1,23 +1,23 @@
 from check50 import *
 
-class First(Checks):
+class Divisible1(Checks):
 
     @check()
     def exists(self):
         """Demo exists"""
-        self.require("First.java")
+        self.require("Divisible1.java")
 
    @check("exists")
    def compiles(self):
-        """First compiles""" 
-        self.spawn("javac First.java").exit(0)
+        """Divisible1 compiles""" 
+        self.spawn("javac Divisible1.java").exit(0)
 
     @check("compiles")
     def test1(self):
-        """input of 1 3 correctly gives output of 0"""
-        self.spawn("java First")..stdin("1 3 ").stdout("0\n", "0\n")
+        """input of 10 5 correctly gives output of DIVISIBLE"""
+        self.spawn("java Divisible1").stdin("10").stdin("5").stdout("DIVISIBLE\n", "DIVISIBLE\n")
 
     @check("compiles")
     def test2(self):
-        """input of 5 3 correctly gives output of 0"""
-        self.spawn("java First").stdin("5 3").stdout("1\n", "1\n")
+        """input of 10 3 correctly gives output of NOT DIVISIBLE"""
+        self.spawn("java Divisible1").stdin("10").stdin("3").stdout("NOT DIVISIBLE\n", "NOT DIVISIBLE\n")
