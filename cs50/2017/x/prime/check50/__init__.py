a9048c6074692cdb423c827e8197b20912b48627
jake23hogs <gilesj@greenbrierschools.org>
2018-09-12 12:05:32 -0500

    Update __init__.py

--- cs50/2017/x/prime/check50/__init__.py
+++ cs50/2017/x/prime/check50/__init__.py
@@ -18,6 +18,21 @@ class Prime(Checks):
         self.spawn("java Prime").stdin("5").stdout("true\n", "true\n")
 
     @check("compiles")
+    def test3(self):
+        """rejects an input of 3"""
+        self.spawn("java Prime").stdin("3").reject()
+
+    @check("compiles")
+    def test8(self):
+        """input of 8 yields output of false"""
+        self.spawn("java Prime").stdin("8").stdout("false\n", "false\n")
+
+    @check("compiles")
+    def test197(self):
+        """input of 197 yields output of true"""
+        self.spawn("java Prime").stdin("197").stdout("true\n", "true\n")
+
+    @check("compiles")
     def test_reject_negative(self):
         """rejects a negative input like -1"""
         self.spawn("java Prime").stdin("-1").reject()
