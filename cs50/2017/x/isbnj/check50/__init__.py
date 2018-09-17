3b21ff5331101e657769ff7cf5cab507fb2ef631
jake23hogs <gilesj@greenbrierschools.org>
2018-09-17 12:19:01 -0500

    Update __init__.py

--- cs50/2017/x/isbnj/check50/__init__.py
+++ cs50/2017/x/isbnj/check50/__init__.py
@@ -13,58 +13,48 @@ class ISBN(Checks):
         """ISBN.java compiles."""
         self.spawn("javac ISBN.java").exit(0)
     
+   
     @check("compiles")
-    def test1(self):
-        """identifies 378282246310005 as AMEX"""
-        self.spawn("java Credit").stdin("378282246310005").stdout("^AMEX\n", "AMEX\n").exit(0)
-    
+    def Absolute_Beginners_Guide(self):
+        """Beginners Guide (0789751984) valid"""
+        self.spawn("javac ISBN.java").stdin("0789751984").stdout("^YES\n", "YES\n").exit(0)
+
     @check("compiles")
-    def test2(self):
-        """identifies 371449635398431 as AMEX"""
-        self.spawn("java Credit").stdin("371449635398431").stdout("^AMEX\n", "AMEX\n").exit(0)
-    
+    def Absolute_Beginners_Guide_fake(self):
+        """Beginners Guide fake (0789751985) invalid"""
+        self.spawn("javac ISBN.java").stdin("0789751985").stdout("^NO\n", "NO\n").exit(0)
+
     @check("compiles")
-    def test3(self):
-        """identifies 5555555555554444 as MASTERCARD"""
-        self.spawn("java Credit").stdin("5555555555554444").stdout("^MASTERCARD\n", "MASTERCARD\n").exit(0)
-    
+    def Programming_in_C(self):
+        """Programming in C (0321776410) valid"""
+        self.spawn("javac ISBN.java").stdin("0321776410").stdout("^YES\n", "YES\n").exit(0)
+
     @check("compiles")
-    def test4(self):
-        """identifies 5105105105105100 as MASTERCARD"""
-        self.spawn("java Credit").stdin("5105105105105100").stdout("^MASTERCARD\n", "MASTERCARD\n").exit(0)
-    
+    def Hackers_Delight(self):
+        """Hackers Delight (0321842685) valid"""
+        self.spawn("javac ISBN.java").stdin("0321842685").stdout("^YES\n", "YES\n").exit(0)
+
     @check("compiles")
-    def test5(self):
-        """identifies 4111111111111111 as VISA"""
-        self.spawn("java Credit").stdin("4111111111111111").stdout("^VISA\n", "VISA\n").exit(0)
-    
+    def phone_number(self):
+        """Jennys number (6178675309) invalid"""
+        self.spawn("javac ISBN.java").stdin("6178675309").stdout("^NO\n", "NO\n").exit(0)
+
     @check("compiles")
-    def test6(self):
-        """identifies 4012888888881881 as VISA"""
-        self.spawn("java Credit").stdin("4012888888881881").stdout("^VISA\n", "VISA\n").exit(0)
-    
+    def memory(self):
+        """Mystery Test"""
+        self.spawn("javac ISBN.java").stdin("1632168146").stdout("^YES\n", "YES\n").exit(0)
+
     @check("compiles")
-    def test7(self):
-        """identifies 1234567890 as INVALID"""
-        self.spawn("java Credit").stdin("1234567890").stdout("^INVALID\n", "INVALID\n").exit(0)
-    
+    def ISBN_with_X(self):
+        """rejects ISBNs with X as checksum"""
+        self.spawn("javac ISBN.java").stdin("078974984X").reject()
+
     @check("compiles")
-    def test8(self):
-        """identifies 5105105105135100 as INVALID"""
-        self.spawn("java Credit").stdin("5105105105135100").stdout("^INVALID\n", "INVALID\n").exit(0)
-    
+    def rejects_ISBNs_with_dashes(self):
+        """rejects ISBNs with dashes"""
+        self.spawn("javac ISBN.java").stdin("0-789-75198-4").reject()
+
     @check("compiles")
-    def test9(self):
-        """identifies 4111111111111114 as INVALID"""
-        self.spawn("java Credit").stdin("4111111111111114").stdout("^INVALID\n", "INVALID\n").exit(0)
-    
-    @check("compiles")
-    def test10(self):
-        """identifies 378282246310006 as INVALID"""
-        self.spawn("java Credit").stdin("378282246310006").stdout("^INVALID\n", "INVALID\n").exit(0)
-    
-    
-    @check("compiles")
-    def test_reject_empty(self):
+    def rejects_empty(self):
         """rejects a non-numeric input of "" """
-        self.spawn("java Credit").stdin("").reject()
+        self.spawn("javac ISBN.java").stdin("").reject()
