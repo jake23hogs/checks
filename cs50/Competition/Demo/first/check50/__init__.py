72f497b00da6fbe9860c91c7d7c0f5eb43e312c3
jake23hogs <gilesj@greenbrierschools.org>
2018-09-18 14:32:49 -0500

    Add files via upload

---
+++ cs50/Competition/Demo/first/check50/__init__.py
@@ -0,0 +1,24 @@
+from check50 import *
+
+
+class Divisible1(Checks):
+
+    @check()
+    def exists(self):
+        """Demo exists"""
+        self.require("Divisible1.java")
+
+    @check("exists")
+    def compiles(self):
+        """Divisible1 compiles""" 
+        self.spawn("javac Divisible1.java").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """input of 10 5 correctly gives output of DIVISIBLE"""
+        self.spawn("java Divisible1").stdin("10").stdin("5").stdout("DIVISIBLE\n", "DIVISIBLE\n")
+
+    @check("compiles")
+    def test2(self):
+        """input of 10 3 correctly gives output of NOT DIVISIBLE"""
+        self.spawn("java Divisible1").stdin("10").stdin("3").stdout("NOT DIVISIBLE\n", "NOT DIVISIBLE\n")
---
+++ cs50/Competition/Demo/second/check50/__init__.py
@@ -0,0 +1,24 @@
+from check50 import *
+
+
+class Divisible2(Checks):
+
+    @check()
+    def exists(self):
+        """Demo exists"""
+        self.require("Divisible2.java")
+
+    @check("exists")
+    def compiles(self):
+        """Divisible2 compiles""" 
+        self.spawn("javac Divisible2.java").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """input of 3 correctly gives output of PRIME"""
+        self.spawn("java Divisible2").stdin("3").stdout("PRIME\n", "PRIME\n")
+
+    @check("compiles")
+    def test2(self):
+        """input of 12 correctly gives output of 2^2 * 3"""
+        self.spawn("java Divisible2").stdin("12").stdout("2^2 * 3\n", "2^2 * 3\n")
