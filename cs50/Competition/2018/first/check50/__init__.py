87e7ea9e3bd32865630d14cdfe7d6a4c1432c272
jake23hogs <gilesj@greenbrierschools.org>
2018-09-18 14:51:21 -0500

    Update __init__.py

--- cs50/Competition/2018/first/check50/__init__.py
+++ cs50/Competition/2018/first/check50/__init__.py
@@ -1,24 +1,24 @@
 from check50 import *
 
 
-class Divisible1(Checks):
+class First(Checks):
 
     @check()
     def exists(self):
-        """Demo exists"""
-        self.require("Divisible1.java")
+        """First.java exists"""
+        self.require("First.java")
 
     @check("exists")
     def compiles(self):
-        """Divisible1 compiles""" 
-        self.spawn("javac Divisible1.java").exit(0)
+        """First compiles""" 
+        self.spawn("javac First.java").exit(0)
 
     @check("compiles")
     def test1(self):
-        """input of 10 5 correctly gives output of DIVISIBLE"""
-        self.spawn("java Divisible1").stdin("10").stdin("5").stdout("DIVISIBLE\n", "DIVISIBLE\n")
+        """input of AABB correctly gives output of ACCEPTED!"""
+        self.spawn("java First").stdin("AABB").stdout("ACCEPTED!\n", "ACCEPTED!\n")
 
     @check("compiles")
     def test2(self):
-        """input of 10 3 correctly gives output of NOT DIVISIBLE"""
-        self.spawn("java Divisible1").stdin("10").stdin("3").stdout("NOT DIVISIBLE\n", "NOT DIVISIBLE\n")
+        """input of BA correctly gives output of REJECTED!"""
+        self.spawn("java First").stdin("BA").stdout("REJECTED!\n", "REJECTED!\n")
