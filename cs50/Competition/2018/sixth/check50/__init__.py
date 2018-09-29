81c2647cee9ce81eeca561f132d6427148d062b5
jake23hogs <gilesj@greenbrierschools.org>
2018-09-28 22:15:49 -0500

    Update __init__.py

--- cs50/Competition/2018/sixth/check50/__init__.py
+++ cs50/Competition/2018/sixth/check50/__init__.py
@@ -1,23 +1,28 @@
 from check50 import *
 
-class First(Checks):
+class Sixth(Checks):
 
     @check()
     def exists(self):
-        """Demo exists"""
-        self.require("First.java")
+        """Sixth exists"""
+        self.require("Sixth.java")
 
    @check("exists")
    def compiles(self):
-        """First compiles""" 
-        self.spawn("javac First.java").exit(0)
+        """Sixth compiles""" 
+        self.spawn("javac Sixth.java").exit(0)
 
     @check("compiles")
     def test1(self):
-        """input of 1 3 correctly gives output of 0"""
-        self.spawn("java First")..stdin("1 3 ").stdout("0\n", "0\n")
+        """input of 011 correctly gives output of INVALID"""
+        self.spawn("java Sixth").stdin("011").stdout("INVALID\n", "INVALID\n")
 
     @check("compiles")
-    def test2(self):
-        """input of 5 3 correctly gives output of 0"""
-        self.spawn("java First").stdin("5 3").stdout("1\n", "1\n")
+    def test1(self):
+        """input of 1111 correctly gives output of YES"""
+        self.spawn("java Sixth").stdin("1111").stdout("YES\n", "YES\n")
+        
+    @check("compiles")
+    def test1(self):
+        """input of 1010 correctly gives output of NO"""
+        self.spawn("java Sixth").stdin("1010").stdout("NO\n", "NO\n")
