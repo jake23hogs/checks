88bf69a94549d0a3621458583be667b1b0afc02a
jake23hogs <gilesj@greenbrierschools.org>
2018-10-01 21:00:40 -0500

    Update __init__.py

--- cs50/Competition/2018/fifth/check50/__init__.py
+++ cs50/Competition/2018/fifth/check50/__init__.py
@@ -14,10 +14,10 @@ class Fifth(Checks):
 
     @check("compiles")
     def test1(self):
-        """input of 2\n3 correctly gives output of 00\n01\n10\n11\n000\n001\n010\n011\n100\n101\n110\n111\n"""
+        """input of 2newline3 correctly gives output of 00newline01newline10newline11newline000newline001newline010newline011newline100newline101newline110newline111newline"""
         self.spawn("java Fifth").stdin("2 3").stdout("00\n01\n10\n11\n000\n001\n010\n011\n100\n101\n110\n111\n", "00\n01\n10\n11\n000\n001\n010\n011\n100\n101\n110\n111\n")
 
     @check("compiles")
     def test2(self):
-        """input of 2\n2 correctly gives output of 00\n01\n10\n11\n"""
+        """input of 2newline2 correctly gives output of 00newline01newline10newline11newline"""
         self.spawn("java Fifth").stdin("2 2").stdout("00\n01\n10\n11\n", "00\n01\n10\n11\n")
