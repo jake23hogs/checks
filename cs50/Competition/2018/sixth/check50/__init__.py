87e472cb9fa46f54e4c12904790467365e6f5207
jake23hogs <gilesj@greenbrierschools.org>
2018-10-01 20:21:03 -0500

    Update __init__.py

--- cs50/Competition/2018/sixth/check50/__init__.py
+++ cs50/Competition/2018/sixth/check50/__init__.py
@@ -18,11 +18,11 @@ class Sixth(Checks):
         self.spawn("java Sixth").stdin("011").stdout("INVALID\n", "INVALID\n")
 
     @check("compiles")
-    def test1(self):
+    def test2(self):
         """input of 1111 correctly gives output of YES"""
         self.spawn("java Sixth").stdin("1111").stdout("YES\n", "YES\n")
         
     @check("compiles")
-    def test1(self):
+    def test3(self):
         """input of 1010 correctly gives output of NO"""
         self.spawn("java Sixth").stdin("1010").stdout("NO\n", "NO\n")
