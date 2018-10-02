d19a49dcd114861228f907f955f6f1c46b203005
jake23hogs <gilesj@greenbrierschools.org>
2018-10-01 20:07:08 -0500

    Update __init__.py

--- cs50/Competition/2018/third/check50/__init__.py
+++ cs50/Competition/2018/third/check50/__init__.py
@@ -15,4 +15,4 @@ class Third(Checks):
     @check("compiles")
     def test1(self):
         """input of 10 5 + 2 3 - - 2 * correctly gives output of 32"""
-        self.spawn("java Third")..stdin("10 5 + 2 3 - - 2 *").stdout("32\n", "32\n")
+        self.spawn("java Third").stdin("10 5 + 2 3 - - 2 *").stdout("32\n", "32\n")
