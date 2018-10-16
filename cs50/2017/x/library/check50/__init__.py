2560be127bdb6e6ceadb8b481e762b5c439ef488
jake23hogs <gilesj@greenbrierschools.org>
2018-10-16 16:04:21 -0500

    Update __init__.py

--- cs50/2017/x/library/check50/__init__.py
+++ cs50/2017/x/library/check50/__init__.py
@@ -3,17 +3,17 @@ from check50 import *
 
 class LibraryTester(Checks):
 
-    @check()
-    def exists(self):
-        """LibraryTester.java, Book.java, and Patron.java exists"""
-	self.require("LibraryTester.java, Book.java, Patron.java")
+    #@check()
+    #def exists(self):
+    #    """LibraryTester.java, Book.java, and Patron.java exists"""
+	#self.require("LibraryTester.java, Book.java, Patron.java")
 	#self.require("Book.java")
 	#self.require("Patron.java")
 	
-    @check("exists")
+    @check()
     def compiles(self):
         """LibraryTester.java compiles"""
-        self.spawn("javac LibraryTester.java").exit(0)
+        self.spawn("javac LibraryTester.java Book.java Patron.java").exit(0)
 
     #@check("compiles")
     #def test1(self):
