39b0887e375a1ba5df35356287590b68bc5cabd3
jake23hogs <gilesj@greenbrierschools.org>
2018-10-26 22:43:12 -0500

    Add files via upload

---
+++ cs50/Competition/2018/tenth/check50/__init__.py
@@ -0,0 +1,24 @@
+from check50 import *
+
+
+class Tenth(Checks):
+
+    @check()
+    def exists(self):
+        """Tenth.java exists."""
+        self.require("Tenth.java")
+
+    @check("exists")
+    def compiles(self):
+        """Tenth.java compiles."""
+        self.spawn("javac Tenth.java").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """Outputs 2707   PRIME"""
+        self.spawn("java Tenth").stdin("1047 4170 2513 2424 2866 4394 2707 4947 22 5405 745 2638 8550 2910 3218 5036 3546 2859").stdout("2707   PRIME\n", "2707   PRIME\n").exit(0)
+
+    @check("compiles")
+    def test2(self):
+        """Outputs 8550   NOT PRIME"""
+        self.spawn("java Tenth").stdin("1047 4170 2513 2424 2866 4394 270 4947 22 5405 745 2638 8550 2910 3218 5036 3546 2859").stdout("8550   NOT PRIME\n", "8550   NOT PRIME\n").exit(0)
