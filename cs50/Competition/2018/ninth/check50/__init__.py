9daec76e6b80e4246f19f02ea613637b7ad25864
jake23hogs <gilesj@greenbrierschools.org>
2018-10-26 22:07:51 -0500

    Add files via upload

---
+++ cs50/Competition/2018/ninth/check50/__init__.py
@@ -0,0 +1,24 @@
+from check50 import *
+
+
+class Ninth(Checks):
+
+    @check()
+    def exists(self):
+        """Ninth.java exists."""
+        self.require("Ninth.java")
+
+    @check("exists")
+    def compiles(self):
+        """Ninth.java compiles."""
+        self.spawn("javac Ninth.java").exit(0)
+
+    @check("compiles")
+    def test1(self):
+        """Outputs 300 for the max number"""
+        self.spawn("java Ninth").stdin("56 300 89 299 254 7 85 98 71 111 221").stdout("300\n", "300\n").exit(0)
+
+    @check("compiles")
+    def test2(self):
+        """Outputs 721 for the max number"""
+        self.spawn("java Ninth").stdin("566 325 721 9 99 654 720 685 38 71 111 221").stdout("721\n", "721\n").exit(0)
