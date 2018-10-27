05b9981ae4e6e0e740f5d88db76da3c3afce611e
jake23hogs <gilesj@greenbrierschools.org>
2018-10-26 23:29:02 -0500

    Update __init__.py

--- cs50/Competition/2018/tenth/check50/__init__.py
+++ cs50/Competition/2018/tenth/check50/__init__.py
@@ -22,3 +22,14 @@ class Tenth(Checks):
     def test2(self):
         """Outputs 8550   NOT PRIME"""
         self.spawn("java Tenth").stdin("1047 4170 2513 2424 2866 4394 270 4947 22 5405 745 2638 8550 2910 3218 5036 2859 a").stdout("8550   NOT PRIME\n", "8550   NOT PRIME\n").exit(0)
+
+    @check("compiles")
+    def test3(self):
+        """Outputs 7331   PRIME"""
+        self.spawn("java Tenth").stdin("1047 4170 2513 7331 2866 4394 2707 4947 22 5405 745 2638 8550 2910 3218 3546 2859 a").stdout("7331   PRIME\n", "7331   PRIME\n").exit(0)
+
+    @check("compiles")
+    def test4(self):
+        """Mystery Check"""
+        self.spawn("java Tenth").stdin("1047 4170 2513 2424 2866 4394 270 4947 22 5405 745 2638 8550 2910 3218 5036 7499 a").stdout("7499   PRIME\n", "7499   PRIME\n").exit(0)
+        
