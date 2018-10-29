1d64f6fb681d163e024600de8400b7c753cc4087
jake23hogs <gilesj@greenbrierschools.org>
2018-10-28 21:32:28 -0500

    Update __init__.py

--- cs50/2017/x/crack/check50/__init__.py
+++ cs50/2017/x/crack/check50/__init__.py
@@ -5,6 +5,21 @@ from check50 import *
 class Crack(Checks):
 
     @check()
-    def submitted(self):
-        """crack submitted"""
-        pass
+    def exists(self):
+        """Crack.java exists."""
+        self.require("Tenth.java")
+    
+    @check("exists")
+    def compiles(self):
+        """Crack.java compiles."""
+        self.spawn("javac Crack.java").exit(0)
+    
+    @check("compiles")
+    def test1(self):
+        """Correctly cracks malan:50CcfIk1QrPr6"""
+        self.spawn("java Crack 50CcfIk1QrPr6").stdout("maybe\n", "maybe\n).exit(0)
+
+    @check("compiles")
+    def test2(self):
+        """Correctly cracks brian:50mjprEcqC/ts"""
+        self.spawn("java Crack 50mjprEcqC/ts").stdout("CA\n", "CA\n).exit(0)
