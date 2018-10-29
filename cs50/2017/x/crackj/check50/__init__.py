46a20f242de17aaa8f868bbec9ae5f252d2f526d
jake23hogs <gilesj@greenbrierschools.org>
2018-10-28 23:15:59 -0500

    Update __init__.py

--- cs50/2017/x/crackj/check50/__init__.py
+++ cs50/2017/x/crackj/check50/__init__.py
@@ -3,15 +3,14 @@ from check50 import *
 
 class Crack(Checks):
         
-   # @check()
-   # def compiles(self):
-   #     """Crack.java compiles"""
-   #     self.spawn("javac Crack.java").exit(0)
+    @check()
+    def compiles(self):
+        """Crack.java compiles"""
+        self.spawn("javac Crack.java B64.java CharEncoding.java Charsets.java Crypt.java DigestUtils.java Hex.java MessageDigestAlgorithms.java Sha2Crypt.java UnixCrypt.java").exit(0)
     
     @check()
     def test1(self):
         """Correctly cracks maria:509nVI8B9VfuA"""
-        self.spawn("javac Crack.java").exit(0)
         self.spawn("java Crack 509nVI8B9VfuA").stdout("TF\n", "TF\n").exit(0)
 
    # @check("compiles")
