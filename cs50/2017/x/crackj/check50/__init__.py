7bb66ec0e280cd7a2cbcebb785e36b60f7604779
jake23hogs <gilesj@greenbrierschools.org>
2018-10-29 00:16:27 -0500

    Update __init__.py

--- cs50/2017/x/crackj/check50/__init__.py
+++ cs50/2017/x/crackj/check50/__init__.py
@@ -16,4 +16,5 @@ class Crack(Checks):
     @check("compiles")
     def test2(self):
         """Correctly cracks brian:50mjprEcqC/ts"""
-        self.spawn("java Crack 50mjprEcqC/ts").stdout("CA\n", "CA\n).exit(0)
+        self.spawn("java Crack 50mjprEcqC/ts").stdout("CA\n", "CA\n").exit(0)
+           
